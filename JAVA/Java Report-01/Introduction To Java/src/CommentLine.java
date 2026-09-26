9768624bb78e48393af2cbd4483c9b0b2e6ad276
253-16-019 <253-16-019@diu.edu.bd>
2026-09-26 19:22:20 +0600

    Intro

---
+++ JAVA/Java Report-01/Introduction To Java/src/CommentLine.java
@@ -0,0 +1,9 @@
+public class CommentLine {
+    public static void main(String[] args) {
+        // -single sine er jonno
+        System.out.println("Hello Rashed !");
+        /*Multipale line
+        comment korar jonno */
+        System.out.println("Its my work");
+    }
+}
